4a73550ee63736b98be5c6d92c2fdc41ff446efc
i0dheerajdileep <i.dheerajdileep@gmail.com>
2023-11-04 14:45:00 +0530

    init tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Landing from './pages/Landing'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='bg-black'>
+      <Landing/>
+    </div>
   )
 }
 
