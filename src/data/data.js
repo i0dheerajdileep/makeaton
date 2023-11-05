4a0512b693a42ab6ec1de305e9fe7d6970bc6d50
Swsthik H Nair <swsthik.nair@gmail.com>
2023-11-05 05:34:53 +0530

    Games Added

--- src/data/data.js
+++ src/data/data.js
@@ -1,19 +1,19 @@
 // highDyslexia.json
 export const highDyslexiaGames = [
-    'https://highdyslexiagame1.com',
-    'https://highdyslexiagame2.com',
+    'https://pisimulation.github.io/WordChallenge',
+    // 'https://highdyslexiagame2.com',
     // Add more high dyslexia game links as needed
 ];
 
 // medDyslexia.json
 export const medDyslexiaGames = [
-    'http://ozluy.github.io/projects/react-hangman',
+    'https://mathtrix.herokuapp.com/',
     // Add more medium dyslexia game links as needed
 ];
 
 // lowDyslexia.json
 export const lowDyslexiaGames = [
-    'https://lowdyslexiagame1.com',
-    'https://lowdyslexiagame2.com',
+    'https://trident09.github.io/Hangman/',
+    //'https://lowdyslexiagame2.com',
     // Add more low dyslexia game links as needed
 ];
